569f66f0ec133916ffb1b9a02737b3fd00057aed
vardaro <anthonyavardaro67@gmail.com>
2020-10-09 23:01:24 -0500

    finish

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 
 'use strict';
 
-const TastyWorks = require('tasty-works-api');
+const tw = require('tasty-works-api');
+const mongo = require('mongodb').MongoClient;
 const credentials = {
     username: process.env.TASTY_USERNAME || require('./secret/secret').TASTY_USERNAME,
     password: process.env.TASTY_PASSWORD || require('./secret/secret').TASTY_PASSWORD
 };
 
-let TASTY_ACCOUNT_ID = process.env.TASTY_ACCOUNT_ID || require('./secret/secret').TASTY_ACCOUNT_ID;
+const TASTY_ACCOUNT_ID = process.env.TASTY_ACCOUNT_ID || require('./secret/secret').TASTY_ACCOUNT_ID;
+const DATABASE_URL = process.env.DATABASE_URL || require('./secret/secret').DATABASE_URL;
 
-TastyWorks.setUser(credentials);
-
-TastyWorks.authorization()
+tw.setUser(credentials);
+console.log(`TW: Connecting to TastyWorks`);
+tw.authorization()
     .then(token => {
-        // Set the authorization in the headers
-        TastyWorks.setAuthorizationToken(token);
-        console.log('Session is active, continue with other calls.');
+        tw.setAuthorizationToken(token);
         return true;
     })
-    .then(() => TastyWorks.balances(TASTY_ACCOUNT_ID))
+    .then(() => tw.balances(TASTY_ACCOUNT_ID))
     .catch(err => console.log(err.status))
     .then(balances => {
-        let netliq = balances['net-liquidating-value'];
-        console.log(netliq);
+        let netliq = Number(balances['net-liquidating-value']);
+        console.log(`TW: Connected to TastyWorks. netliq = ${netliq}`);
+        console.log(`TW: Connecting to DB`);
+        mongo.connect(DATABASE_URL, { useUnifiedTopology: true }, function (err, client) {
+            console.log("TW: Connecting successfully to DB");
+
+            const db = client.db('account_value');
+
+            const collection = db.collection('documents');
+            // Insert some documents
+            collection.insertOne(
+                { account_value: netliq, date: new Date() },
+                function (err, result) {
+                    if (err)
+                        throw err;
+
+                    console.log(`TW: Insert successful`);
+                    client.close();
+                });
+        });
+
     });
